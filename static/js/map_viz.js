4d6b06e7d9d9d32261b1f970d1ade60a52156047
Kevin Trieu <dragn194@gmail.com>
2018-12-18 14:32:14 -0500

    Add pan and zoom functionality
    It is obscenely slow at high zoom levels. May have to simplify the file more or pull out some tricks.

--- static/js/map_viz.js
+++ static/js/map_viz.js
@@ -12,49 +12,64 @@ class VectorMap {
         var root_rect = this.root.getBoundingClientRect();
         this.width = root_rect.width;
         this.height = 720; 
+   
+        this.zoom = d3.zoom()
+            .on('zoom', _ => this.onZoom(d3.event.transform));
 
-        var projection = d3.geoMercator()
-            .center(map_center)
-            .scale((1 << 30) / this.width / 2 / Math.PI)
-            .translate([this.width / 2, this.height / 2])
-            .precision(0);
+        this.svg = d3.select(this.root).append('svg')
+            .attr('width', this.width)
+            .attr('height', this.height)
+            .call(this.zoom);
 
+        this.projection = d3.geoMercator()
+            .scale(1 / 2 / Math.PI)
+            .translate([0, 0]);
+
+        this.path = d3.geoPath()
+            .projection(this.projection);
+    }
+
+    onZoom(transform) {
+
+        //save variables because d3 replaces 'this'
+        var path = this.path;
+        
         var tiler = d3.tile()
             .size([this.width, this.height])
-            .scale(projection.scale() * 2 * Math.PI)
-            .translate(projection([0, 0]))
+            .translate([transform.x, transform.y])
+            .scale(transform.k)
+
+        var groups = this.svg.selectAll('g')
+            .attr('transform', `translate(${transform.x},${transform.y}) scale(${transform.k})`)
+            .data(tiler, function (d) { return [d[0], d[1], d[2]]});
+
+        groups.enter().append('g')
+            .each(function (tile) {
+                var g = d3.select(this);
+                var zoom = tile[2];
+                var x = tile[0];
+                var y = tile[1];
+                g.attr('transform', `translate(${transform.x},${transform.y}) scale(${transform.k})`);
+                g.attr('class', `${zoom}-${x}-${y}`);
+                if (zoom > 10) {
+                    // since these are vector tiles we can just overzoom the z10 ones
+                    x = Math.floor(x / Math.pow(2, zoom - 10));
+                    y = Math.floor(y / Math.pow(2, zoom - 10));
+                    zoom = 10;
+                }
+                d3.json(`/maps/${zoom}/${x}/${y}/`)
+                    .then(function(json) {
+                        g.selectAll('path')
+                            .data(json.features)
+                        .enter()
+                            .append('path')
+                            .attr('d', path)
+                            .attr('class', 'poll-feature')
+                            .attr('vector-effect',  'non-scaling-stroke')
+                    });
+            });
         
-        var path = d3.geoPath()
-            .projection(projection);
-
-        var svg = d3.select(this.root).append('svg')
-            .attr('width', this.width)
-            .attr('height', this.height);
-
-        svg.selectAll('g')
-            .data(tiler)
-            .enter().append('g')
-                .each(function (tile) {
-                    var g = d3.select(this);
-                    var zoom = tile[2];
-                    var x = tile[0];
-                    var y = tile[1];
-                    if (zoom > 10) {
-                        // since these are vector tiles we can just overzoom the z10 ones
-                        x = Math.floor(x / Math.pow(2, zoom - 10));
-                        y = Math.floor(y / Math.pow(2, zoom - 10));
-                        zoom = 10;
-                    }
-                    d3.json(`/maps/${zoom}/${x}/${y}/`)
-                        .then(function(json) {
-                            console.log(g);
-                            g.selectAll('path')
-                                .data(json.features)
-                            .enter().append('path')
-                                .attr('d', path)
-                                .attr('class', 'poll-feature')
-                        });
-                });
+        groups.exit().remove();
     }
 }
 
