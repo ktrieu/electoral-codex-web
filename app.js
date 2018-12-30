cc1626bd44ae2bfd0bf64b1f4d92c7d5e01fd6a4
Kevin Trieu <dragn194@gmail.com>
2018-12-30 00:33:11 -0500

    Switch to using Mapbox instead of the homegrown vector tiles
    This is just infinitely easier than rolling my own map.

--- app.js
+++ app.js
@@ -1,32 +1,15 @@
 var express = require('express');
 var handlebars = require('express-handlebars');
 
-var maps = require('./maps');
-
-maps.load_all_mbtiles();
-
 var app = express();
 
-app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.engine('handlebars', handlebars({defaultLayout: 'base'}));
 app.set('view engine', 'handlebars');
 
 app.use('/static', express.static('static/'));
 
 app.get('/', function(req, res) {
-    res.render('home', {layout: 'base'});
-});
-
-app.get('/maps/:zoom/:x/:y/', function(req, res) {
-    maps.get_tile_as_JSON(
-        'polls_2004.mbtiles', req.params.zoom, req.params.x, req.params.y, 
-        (err, tile_json) => {
-            if (err) {
-                res.send(err);
-            }
-            else {
-                res.send(tile_json);
-            }
-        });
+    res.render('home', { mapbox_key : process.env.MAPBOX_KEY });
 });
 
 app.listen(3000);
