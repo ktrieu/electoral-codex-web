ede73113912086ab1dd7617311aa9b09ee096943
Kevin Trieu <dragn194@gmail.com>
2019-01-03 17:43:09 -0500

    Add basic riding map popups

--- static/js/election-main.js
+++ static/js/election-main.js
@@ -1,3 +1,9 @@
+updatePopup = function(popup, e) {
+    var ridingProps = e.features[0].properties;
+    popup.setHTML(`<h1>${ridingProps.N}</h1>`)
+        .setLngLat(e.lngLat)
+}
+
 $(document).ready(function () {
     mapboxgl.accessToken = MAPBOX_KEY;
     var mapboxMap = new mapboxgl.Map({
@@ -5,5 +11,23 @@ $(document).ready(function () {
         style: 'mapbox://styles/kevintrieu/cjq93yzad9fpm2rocm2a27hqi',
         center: [-103.338053, 60.772455],
         zoom: 2.8
+    });
+
+    var ridingPopup = new mapboxgl.Popup({
+        closeButton: false,
+        closeOnClick: false
+    });
+
+    mapboxMap.on('mouseenter', 'ridings-fill', function(e) {
+        ridingPopup.addTo(mapboxMap);
+        updatePopup(ridingPopup, e);
+    });
+
+    mapboxMap.on('mouseleave', 'ridings-fill', function(e) {
+        ridingPopup.remove();
+    });
+
+    mapboxMap.on('mousemove', 'ridings-fill', function(e) {
+        updatePopup(ridingPopup, e);
     });
 });
