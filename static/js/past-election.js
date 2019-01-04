b8b9d5b14c20ceb74f062507f019fd896c76e8ff
Kevin Trieu <dragn194@gmail.com>
2019-01-03 23:42:46 -0500

    Add riding data to the map popups

--- static/js/past-election.js
+++ static/js/past-election.js
@@ -1,7 +1,63 @@
+popupContent = function(ridingData) {
+    return `
+    <div class="popup-content">
+        <h4 class="border-bottom">${ridingData.name}</h3>
+        <div class="row">
+            <div class="col-sm">
+                <p>${ridingData.results[0].party}</p>
+            </div>
+            <div class="col-sm text-right">
+                <p>${ridingData.results[0].percent}%</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm">
+                <p>${ridingData.results[1].party}</p>
+            </div>
+            <div class="col-sm text-right">
+                <p>${ridingData.results[1].percent}%</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm">
+                <p>${ridingData.results[2].party}</p>
+            </div>
+            <div class="col-sm text-right">
+                <p>${ridingData.results[2].percent}%</p>
+            </div>
+        </div>
+    </div>
+    `
+}
+
 updatePopup = function(popup, e) {
     var ridingProps = e.features[0].properties;
-    popup.setHTML(`<h1>${ridingProps.N}</h1>`)
-        .setLngLat(e.lngLat)
+    popup.setLngLat(e.lngLat)
+    //extract the vote numbers
+    var votePairs = [];
+    var totalVote = 0;
+    for (var key in ridingProps) {
+        var items = key.split('-');
+        if (items[0] === 'V') {
+            votePairs.push([items[1], ridingProps[key]]);
+            totalVote += ridingProps[key];
+        }
+    }
+    //sort by vote count
+    votePairs.sort((a, b) => a[1] < b[1]);
+    var resultArray = [];
+    for (var i = 0; i < 3; i++) {
+        resultArray.push({
+            party : votePairs[i][0],
+            percent : (100 * votePairs[i][1] / totalVote).toFixed(2)
+        });
+    }
+    var ridingData = {
+        //replace the double hyphens with single ones
+        name : ridingProps.N.replace(/--/g, '-'),
+        results : resultArray
+    }
+    popup.setHTML(popupContent(ridingData));
 }
 
 $(document).ready(function () {
