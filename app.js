8502e4f4a25528e5871e14d8978f8a8abe968317
Kevin Trieu <dragn194@gmail.com>
2019-01-03 20:18:04 -0500

    Do some template housekeeping to get per template CSS and JS working

--- app.js
+++ app.js
@@ -3,13 +3,26 @@ var handlebars = require('express-handlebars');
 
 var app = express();
 
-app.engine('handlebars', handlebars({defaultLayout: 'base'}));
+var handlebarOpts = {
+    defaultLayout: 'base',
+    helpers : {
+        json : function(ctx) { JSON.stringify(ctx) }
+    }
+}
+
+app.engine('handlebars', handlebars(handlebarOpts));
 app.set('view engine', 'handlebars');
 
 app.use('/static', express.static('static/'));
 
+renderTemplate = function(res, template, params) {
+    var context = { customCss : [template], customJs : [template] };
+    Object.assign(context, params);
+    res.render(template, context);
+}
+
 app.get('/', function(req, res) {
-    res.render('home', { mapbox_key : process.env.MAPBOX_KEY });
+    renderTemplate(res, 'past-election', { mapbox_key : process.env.MAPBOX_KEY });
 });
 
 app.listen(3000);
